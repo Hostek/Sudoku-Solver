216eaa572f410fa5c79a59138f0233bc75308085
Hostek <michalpl245674477@gmail.com>
2022-12-25 17:34:35 +0100

    Added input boxes

--- client/src/components/pages/HomePage.tsx
+++ client/src/components/pages/HomePage.tsx
@@ -4,13 +4,41 @@ import NavBar from "../NavBar"
 
 interface HomePageProps {}
 
+const LocalInnerSquare: React.FC = () => {
+    return (
+        <div className="inner-square">
+            <input type="number" />
+            <input type="number" />
+            <input type="number" />
+            <input type="number" />
+            <input type="number" />
+            <input type="number" />
+            <input type="number" />
+            <input type="number" />
+            <input type="number" />
+        </div>
+    )
+}
+
 const HomePage: React.FC<HomePageProps> = ({}) => {
     return (
         <>
             <NavBar />
             <main>
                 <div className="game-container">
-                    <div className="game"></div>
+                    <div className="game">
+                        <LocalInnerSquare />
+                        <LocalInnerSquare />
+                        <LocalInnerSquare />
+
+                        <LocalInnerSquare />
+                        <LocalInnerSquare />
+                        <LocalInnerSquare />
+
+                        <LocalInnerSquare />
+                        <LocalInnerSquare />
+                        <LocalInnerSquare />
+                    </div>
                     <button className="btn">SOLVE!</button>
                 </div>
             </main>
